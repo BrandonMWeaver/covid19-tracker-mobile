50a40e9eae157f0c455b196b5356a1668a6df36b
Brandon Weaver <a7xncob@gmail.com>
2021-01-01 19:12:34 -0500

    Implement dashboard component request.

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class Dashboard extends Component {
+    state = {
+        cases: 0,
+        deaths: 0,
+        recovered: 0
+    }
+
+    componentDidMount() {
+        fetch("https://covid19-api.org/api/status/us")
+        .then(r => r.json())
+        .then(o => {
+            this.setState({
+                cases: o.cases,
+                deaths: o.deaths,
+                recovered: o.recovered
+            });
+        });
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Dashboard</Text>
+                <Text>Cases: {this.state.cases}</Text>
+                <Text>Deaths: {this.state.deaths}</Text>
+                <Text>Recovered: {this.state.recovered}</Text>
             </View>
         );
     }
